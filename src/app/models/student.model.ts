8ba270f8077354f26fd4aee16057122dce64d59d
sbinder <sbinder@stdesign.com>
2017-08-09 18:05:38 -0400

    added student model (not working)

---
+++ src/app/models/student.model.ts
@@ -0,0 +1,22 @@
+export class Student {
+
+  STID: number;
+  Org: number;
+  Target: Date;
+  LName: string;
+  FName: string;
+  Gender: string;
+  Parent: number;
+  Teacher: number;
+  Email: string;
+  Aux1: string;
+  Aux2: string;
+  Aux3: string;
+  Aux4: string;
+  Username: string;
+  Password: string;
+  Expires: Date;
+  Trial: boolean;
+  Group: number;
+  selected: boolean;
+}
