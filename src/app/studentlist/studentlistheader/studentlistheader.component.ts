37d85c821b11923ba63915e353cf91238f512f24
sbinder <sbinder@stdesign.com>
2017-08-10 16:38:31 -0400

    style group header buttons

--- src/app/studentlist/studentlistheader/studentlistheader.component.ts
+++ src/app/studentlist/studentlistheader/studentlistheader.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { log } from "util";
 
 @Component({
   selector: 'app-studentlistheader',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentlistheaderComponent implements OnInit {
 
+  groupA = false;
+  groupB = false;
+  grcouAB = false;
+  groupN = false;
+
   constructor() { }
+
+  getClass(btn: string) {
+    let bn = false;
+    switch(btn) {
+      case 'A':
+        bn = this.groupA;
+        break;
+      case 'B':
+        bn = this.groupB;
+    }
+    if (bn) {
+      return 'btn btn-primary';
+    } else {
+      return 'btn btn-default';
+    }
+  }
+
+  bclick(event) {
+    console.log(event.target.value);
+    switch(event.target.value) {
+      case 'A':
+        this.groupA = !this.groupA;
+        break;
+      case 'B':
+        this.groupB = !this.groupB;
+    }
+  }
 
   ngOnInit() {
   }
-
 }
