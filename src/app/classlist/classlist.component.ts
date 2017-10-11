17edd6dccafe4ab924f536c5baac2758c34f5ca4
Stuart Binder <sbinder@stdesign.com>
2017-10-10 22:57:38 -0400

    implemented class list for lessons

--- src/app/classlist/classlist.component.ts
+++ src/app/classlist/classlist.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { StudentsService } from '../services/students.service';
 import { Student } from '../models/student.model';
 
@@ -9,14 +9,45 @@ import { Student } from '../models/student.model';
 })
 export class ClasslistComponent implements OnInit {
 
+  lastdate: string;
   students: Student[];
-  constructor(private studentService: StudentsService) { }
+  constructor(private studentService: StudentsService,
+    private changes: ChangeDetectorRef) { }
 
   ngOnInit() {
     this.studentService.allStudentRequest()
-    .subscribe(
+      .subscribe(
       res => {
-        console.log('preloading students', res);
+        this.students = res.slice();
+        this.changes.detectChanges();
+      });
+  }
+
+  checkDate(newdate) {
+    if (newdate === this.lastdate) {
+      return false;
+    }
+    this.lastdate = newdate;
+    return true;
+  }
+
+  getdate(ds: string) {
+    const d = new Date(+ds.substr(0, 4), +ds.substr(5, 2), +ds.substr(7, 2));
+    return d;
+  }
+
+  gotClicked(id) {
+    this.students.forEach(element => {
+      if (element.stid === id) {
+        element.selected = !element.selected;
+      }
     });
+  }
+
+  isSelection() {
+    for (let i = 0; i < this.students.length; i++) {
+      if (this.students[i].selected) { return true; }
+    }
+    return false;
   }
 }
