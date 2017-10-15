77b06fd7b62ca0dc6c3f3659b819a59b38681c8f
Stuart Binder <sbinder@stdesign.com>
2017-10-15 15:43:10 -0400

    fixed date display in lesson mode

--- src/app/classlist/classlist.component.ts
+++ src/app/classlist/classlist.component.ts
@@ -35,7 +35,7 @@ export class ClasslistComponent implements OnInit {
 
   getdate(ds: string) {
     console.log('making date from', ds);
-    const d = new Date(+ds.substr(0, 4), +ds.substr(5, 2), +ds.substr(7, 2));
+    const d = new Date(+ds.substr(0, 4), +ds.substr(5, 2) - 1, +ds.substr(8, 2));
     return d;
   }
 
